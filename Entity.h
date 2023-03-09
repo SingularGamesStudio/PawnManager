d2ee37cc0dccf5faa8e03df86ed264882e755b1b
SingularGamesStudio <40793299+SingularGamesStudio@users.noreply.github.com>
2023-03-09 13:34:14 +0300

    init commit

---
+++ Entity.h
@@ -0,0 +1,13 @@
+class Entity {
+private:
+    virtual void destroy(){}
+
+public:
+    unsigned int hp;
+    Player owner;
+    void changeHealth(int delta){}
+
+    std::pair<double, double> position;
+    double radius;
+};
+
