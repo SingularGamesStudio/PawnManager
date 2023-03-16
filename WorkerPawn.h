425754df8737a0c02034157bb0dba940cb1e5fec
vertignesss <proventus111@mail.ru>
2023-03-16 18:10:12 +0300

    divided WorkerPawn fixed

--- WorkerPawn.h
+++ WorkerPawn.h
@@ -1,7 +1,4 @@
 #include "Pawn.h"
-#include "Building.h"
-#include "Task.h"
-#include "Entity.h"
 #include <set>
 #include <unordered_map>
 #include <queue>
@@ -11,84 +8,12 @@ class WorkerPawn: public Pawn{
 public:
     Building* positionBuilding;
     
-    void create(Building* placeOfCreation) {
-        currentTask = Task(TaskID::Idle, placeOfCreation);
-        travelling = false;
-        holding = nullptr;
-        owner = placeOfCreation->owner;
-        destination = placeOfCreation;
-        inside = placeOfCreation;
-    }
+    void create(Building* placeOfCreation);
 
-    void assignInnerTask(Task toAssign){
-        currentTask = toAssign;
-        switch(toAssign.id){
-            case TaskID::Get:
-                takeResourceFromBuilding(toAssign.destination, toAssign.object);
-            case TaskID::Transport:
-                takeResourceFromBuilding(toAssign.destination, toAssign.object);
-                moveResorceTo(toAssign.destination2);
-            case TaskID::Move:
-                moveTo(toAssign.destination);
-            case TaskID::BeIngridient:
-                moveTo(toAssign.destination);
-            default:
-                throw("Unexpected WorkerPawn TaskID: ", toAssign.id);
-        }
-    }
+    void assignInnerTask(Task toAssign);
     std::set<expertisesID> expertises;
-    void moveToBuilding(Building* dest){
-        travelling = true;
-        std::unordered_map<Building*, std::vector<Building*> > visited;
-        std::queue<Building*> q;
-        destination = dest;
-        q.push(positionBuilding);
-        while(!q.empty()){
-            Building* currentB = q.front();
-            q.pop();
-            std::vector<Building*> way = visited[currentB];
-            for(Building* toGo:currentB->children)
-            {
-                if(!visited[toGo].empty())
-                    continue;
-                visited[toGo] = way;
-                visited[toGo].push_back(toGo);
-                q.push(toGo);
-            }
-            if(!visited[currentB->parent].empty())
-                continue;
-            visited[currentB->parent] = way;
-            visited[currentB->parent].push_back(currentB->parent);
-            q.push(currentB->parent);
-        }
-        for(Building* v : visited[dest])
-        {
-            moveToNeighbour(v);
-        }
-        travelling = false;
-    }
-    void moveToNeighbour(Building* dest){
-        double currentTime = 0;
-        double timePerMove = sqrt(abs(position.first - dest->position.first) * abs(position.first - dest->position.first) +
-            abs(position.second - dest->position.second) * abs(position.second - dest->position.second));
-        while(currentTime < timePerMove)
-        {
-            currentTime += timePerMove / ticksPerSecond;
-            position.first = currentTime / ticksPerSecond * (*positionBuilding).position.first
-                             + (1 - currentTime / ticksPerSecond) * (*positionBuilding).position.first;
-            position.second = currentTime / ticksPerSecond * (*dest).position.second
-                             + (1 - currentTime / ticksPerSecond) * (*dest).position.second;
-        }
-        positionBuilding = dest;
-        position = dest->position;
-    }
-    void takeResourceFromBuilding(Building* dest, Resource res) {
-        moveToBuilding(dest);
-        dest->resources.erase(dest->resources.find(res));
-        holding = res;
-    }
-    void moveResourceTo(Building* dest) {
-        moveToBuilding(dest);
-        drop();
-    }
+    void moveToBuilding(Building* dest);
+    void moveToNeighbour(Building* dest);
+    void takeResourceFromBuilding(Building* dest, Resource res);
+    void moveResourceTo(Building* dest);
 };
