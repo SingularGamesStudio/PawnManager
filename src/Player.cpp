3fd86315487e9a1f3de465459c822840947f432f
Kiryanov Grigory <40793299+SingularGamesStudio@users.noreply.github.com>
2023-03-16 23:06:09 +0300

    added recipe rosources check

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,5 +1,41 @@
 #include "Player.h"
+#include "Entities/Buildings/Building.h"
+#include <set>
+#include <vector>
 
-void Player::startRecipe(Recipe recipe, Building* where) {
+void dfs(Building* vertex, std::multiset<Resource>& resources){
+    if(resources.size()<vertex->resources.size()){
+        std::vector<Resource> toDelete;
+        for(Resource r:resources) {
+            if(vertex->resources.contains(r)){
+                toDelete.push_back(r);
+            }
+        }
+        for(Resource r:toDelete){
+            resources.erase(resources.find(r));
+        }
+    } else {
+        for(Resource r:vertex->resources) {
+            if(resources.contains(r))
+                resources.erase(resources.find(r));
+        }
+    }
+    for(Building* next:vertex->children) {
+        if (resources.empty())
+            return;
+        dfs(next, resources);
+    }
+}
+
+bool Player::checkRecipe(Recipe recipe) {
+    std::multiset<Resource> resources;
+    for(Resource r:recipe.inResources){resources.insert(r);}
+    dfs(hub, resources);
+    if(!resources.empty())
+        return false;
+
+}
+
+bool Player::startRecipe(Recipe recipe, Building* where) {
 
 }
--- src/Player.h
+++ src/Player.h
@@ -1,20 +1,24 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-///misha does not believe in me  :(
-//#include "Pawn.h"
-//#include "Task.h"
+
 #include "Recipes/Recipe.h"
 #include <vector>
 
 class Building;
 class Pawn;
 
+class PendingTask {
+
+};
+
 class Player {
 public:
     Building* hub;
     std::vector<Pawn*> pawns;
-//    std::vector<>
+    std::vector<PendingTask> tasks;
 
-    void startRecipe(Recipe recipe, Building* where);
+    bool startRecipe(Recipe recipe, Building* where);
+
+    bool checkRecipe(Recipe recipe);
 };
 #endif //PLAYER_H
