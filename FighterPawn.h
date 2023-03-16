f8837b883915a7f90d379c01090f4415bbf00d3a
vertignesss <proventus111@mail.ru>
2023-03-16 19:31:54 +0300

    maybe2

--- FighterPawn.h
+++ FighterPawn.h
@@ -26,13 +26,11 @@ public:
 class DummyMonk : public FighterPawn {
 public:
     void attack(Entity* attacked) override;
-    DummyMonk();
     FighterPawnType getType() override;
 };
 class DummySwordsman : public FighterPawn {
 public:
     void attack(Entity* attacked) override;
-    DummySwordsman();
     FighterPawnType getType() override;
 };
 #endif //FIGHTERPAWN_H
