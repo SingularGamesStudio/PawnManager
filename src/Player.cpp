b218263d5f7679f1b40c4461dab6917d02d3b4ca
SingularGamesStudio <kirgrisha04@mail.ru>
2023-03-17 13:33:28 +0300

    recipe avaliability check done

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,25 +1,16 @@
 #include "Player.h"
 #include "Entities/Buildings/Building.h"
+#include "Entities/Pawns/WorkerPawn.h"
+#include "Entities/Pawns/FighterPawn.h"
 #include <set>
 #include <vector>
 
 void dfs(Building* vertex, std::multiset<Resource>& resources){
-    if(resources.size()<vertex->resources.size()){
-        std::vector<Resource> toDelete;
-        for(Resource r:resources) {
-            if(vertex->resources.contains(r)){
-                toDelete.push_back(r);
-            }
-        }
-        for(Resource r:toDelete){
+    for(Resource r:vertex->resources) {
+        if(resources.contains(r))
             resources.erase(resources.find(r));
-        }
-    } else {
-        for(Resource r:vertex->resources) {
-            if(resources.contains(r))
-                resources.erase(resources.find(r));
-        }
     }
+
     for(Building* next:vertex->children) {
         if (resources.empty())
             return;
@@ -27,15 +18,36 @@ void dfs(Building* vertex, std::multiset<Resource>& resources){
     }
 }
 
-bool Player::checkRecipe(Recipe recipe) {
+bool Player::checkRecipe(Recipe& recipe) {
     std::multiset<Resource> resources;
+    std::multiset<FighterPawnType> fighters;
+    std::multiset<expertisesID> workers;
     for(Resource r:recipe.inResources){resources.insert(r);}
+    for(FighterPawnType t:recipe.inFighters){fighters.insert(t);}
+    for(expertisesID t:recipe.inWorkers){workers.insert(t);}
+    for(expertisesID t:recipe.reqWorkers){workers.insert(t);}
     dfs(hub, resources);
     if(!resources.empty())
         return false;
-
+    for(Pawn* pawn:pawns){
+        if(WorkerPawn* worker = dynamic_cast<WorkerPawn*>(pawn); worker!=nullptr) {
+            for(expertisesID e:worker->expertises){//TODO:change to mincost
+                if(workers.contains(e)){
+                    workers.erase(workers.find(e));
+                    break;
+                }
+            }
+        } else if(FighterPawn* fighter = dynamic_cast<FighterPawn*>(pawn); fighter!=nullptr) {
+            if(fighters.contains(fighter->getType()))
+                fighters.erase(fighters.find(fighter->getType()));
+        }
+    }
+    if(!fighters.empty() || !workers.empty())
+        return false;
+    return true;
 }
 
 bool Player::startRecipe(Recipe recipe, Building* where) {
-
+    if(!checkRecipe(recipe))
+        return false;
 }
