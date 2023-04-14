f4e3e914e5565f6769613e45e60cee96d3a494c8
Dubvas <vasyadbnn@yandex.ru>
2023-04-14 18:07:58 +0300

    added new events

--- src/Event.cpp
+++ src/Event.cpp
@@ -4,32 +4,52 @@
 
 #include "Entities/Buildings/Building.h"
 #include "Entities/Pawns/Pawn.h"
+#include "Player.h"
 #include "IDmanager.h"
-
+#include "Entities/ResourceEntity.h"
 
 Event::Event(Event::Type t, int id) : p(dlib::Packet::Type::RAW_MESSAGE) {
-    if (t != Event::Type::BUILDING_APPEAR && t != Event::Type::BUILDING_DISAPPEAR && t != Event::Type::PAWN_APPEAR &&
-        t != Event::Type::BUILDING_DISAPPEAR && t != Event::Type::PAWN_LET_RES)
+    std::set<Event::Type> acceptable_events;
+    acceptable_events.insert(Event::Type::BUILDING_APPEAR);
+    acceptable_events.insert(Event::Type::BUILDING_DISAPPEAR);
+    acceptable_events.insert(Event::Type::PAWN_APPEAR);
+    acceptable_events.insert(Event::Type::PAWN_DISAPPEAR);
+    acceptable_events.insert(Event::Type::PAWN_LET_RES);
+    acceptable_events.insert(Event::Type::PLAYER_APPEAR);
+    acceptable_events.insert(Event::Type::PLAYER_DISAPPEAR);
+    acceptable_events.insert(Event::Type::RESOURCE_ENTITY_APPEAR);
+    acceptable_events.insert(Event::Type::RESOURCE_ENTITY_DISAPPEAR);
+    if (!acceptable_events.contains(t))
         throw std::invalid_argument("Trying to make event with wrong type");
     std::vector<uint8_t> tmp(sizeof(t));
     std::memcpy(tmp.data(), &t, sizeof(t));
+    std::vector<uint8_t> curr_data;
     if (t == Event::Type::BUILDING_APPEAR) {
         ptr<Building> bu(id);
-        auto bu_s = bu->serialize();
-        std::copy(bu_s.begin(), bu_s.end(), std::back_inserter(tmp));
+        curr_data = bu->serialize();
     } else if (t == Event::Type::PAWN_APPEAR) {
         ptr<Pawn> pw(id);
-        auto pw_s = pw->serialize();
-        std::copy(pw_s.begin(), pw_s.end(), std::back_inserter(tmp));
+        curr_data = pw->serialize();
+    } else if (t == Event::Type::PLAYER_APPEAR) {
+        ptr<Player> pl(id);
+        curr_data = pl->serialize(); // todo
+    } else if (t == Event::Type::RESOURCE_ENTITY_APPEAR) {
+        ptr<ResourceEntity> pl(id);
+        curr_data = pl->serialize(); // todo
     } else {
         tmp.resize(sizeof(t) + sizeof(id));
         std::memcpy(tmp.data() + sizeof(t), &id, sizeof(id));
     }
+    std::copy(curr_data.begin(), curr_data.end(), std::back_inserter(tmp));
     p << tmp;
 }
 
 Event::Event(Event::Type t, int id, Resource res) : p(dlib::Packet::Type::RAW_MESSAGE) {
-    if (t != Event::Type::BUILDING_ADD_RES && t != Event::Type::BUILDING_REMOVE_RES && t != Event::Type::PAWN_TAKE_RES)
+    std::set<Event::Type> acceptable_events;
+    acceptable_events.insert(Event::Type::BUILDING_ADD_RES);
+    acceptable_events.insert(Event::Type::BUILDING_REMOVE_RES);
+    acceptable_events.insert(Event::Type::PAWN_TAKE_RES);
+    if (!acceptable_events.contains(t))
         throw std::invalid_argument("Trying to make event with wrong type");
     std::vector<uint8_t> tmp(sizeof(t) + sizeof(id) + sizeof(res));
     std::memcpy(tmp.data(), &t, sizeof(t));
@@ -39,7 +59,8 @@ Event::Event(Event::Type t, int id, Resource res) : p(dlib::Packet::Type::RAW_ME
 }
 
 Event::Event(Event::Type t, int id, std::pair<double, double> pos) : p(dlib::Packet::Type::RAW_MESSAGE) {
-    if (t != Event::Type::PAWN_MOVE) throw std::invalid_argument("Trying to make event with wrong type");
+    if (t != Event::Type::PAWN_MOVE)
+        throw std::invalid_argument("Trying to make event with wrong type");
     std::vector<uint8_t> tmp(sizeof(t) + sizeof(id) + sizeof(pos));
     std::memcpy(tmp.data(), &t, sizeof(t));
     std::memcpy(tmp.data() + sizeof(t), &id, sizeof(id));
