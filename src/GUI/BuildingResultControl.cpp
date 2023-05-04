31ae2ebd82ae9a688076829fdd4862aad27bab7b
SingularGamesStudio <40793299+SingularGamesStudio@users.noreply.github.com>
2023-05-04 18:53:47 +0300

    Revert "Revert "Add result building rendering in build building window""
    This reverts commit e7082e688f919b282a776629571e36e1295e5065.

--- src/GUI/BuildingResultControl.cpp
+++ src/GUI/BuildingResultControl.cpp
@@ -3,5 +3,15 @@
 //
 
 #include "BuildingResultControl.h"
-BuildingResultControl::BuildingResultControl(GameWindow &win, const sf::IntRect &rect) : Control(win, rect) {}
-void BuildingResultControl::updateAndRender() { Control::updateAndRender(); }
+#include "GameWindow.h"
+#include "PawnManagerClient.h"
+
+BuildingResultControl::BuildingResultControl(GameWindow &win, const sf::IntRect &rect) : Control(win, rect), idea(nullptr) {}
+void BuildingResultControl::updateAndRender() {
+    Control::updateAndRender();
+    sf::FloatRect box = win.getGridRectangle(rect);
+    sf::Vector2f center = box.getPosition() + box.getSize() * (1 / 2.0f);
+    if(idea) {
+        PawnManagerClient::buildingRenderer->drawBuilding(*idea, center);
+    }
+}
